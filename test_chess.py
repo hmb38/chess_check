2f07f4ad1e3736d953013f152c40d924a5bcf057
HMB <johndoe@example.com>
2023-02-24 21:55:30 +0000

    added first unit test for parse_concise_board

---
+++ test_chess.py
@@ -0,0 +1,29 @@
+import unittest
+from chess import parse_concise_board
+
+
+class TestAssertions(unittest.TestCase):
+
+    def test_equals(self):
+        self.assertEqual("one string", "one string")
+
+    def test_parse_concise_board(self):
+        board = (
+            "..k.....",
+            "........",
+            "........",
+            "........",
+            "........",
+            "........",
+            "........",
+            "....K..."
+        )
+
+        self.assertEqual(parse_concise_board(board), [
+            {"type": "King", "colour": "White", "pos": {"x": 3, "y": 1}},
+            {'type': 'King', 'colour': 'Black', 'pos': {'x': 5, 'y': 8}}
+        ])
+
+
+if __name__ == '__main__':
+    unittest.main()
